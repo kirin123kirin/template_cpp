84cbf6fa229a2406caaeecb272655e5bee6faaf4
kirin123 <3436009+kirin123kirin@users.noreply.github.com>
2023-12-25 16:12:58 +0900

    Update init.cmd

--- init.cmd
+++ init.cmd
@@ -1,8 +1,10 @@
-git clone https://github.com/kirin123kirin/__PROJECT_NAME__.git
-cd __PROJECT_NAME__
-git submodule update --init --remote --recursive
-git rm --cached .github
-rm -rf .git\modules\.github
-rm .github\.git
+# remove all submodules (`git rm`) from .gitmodules
+git submodule | cut -c43- | while read -r line; do (git rm "$line"); done
+
+# delete all submodule sections from .git/config (`git config --local --remove-section`) by fetching those from .git/config
+git config --local -l | grep submodule | sed -e 's/^\(submodule\.[^.]*\)\(.*\)/\1/g' | while read -r line; do (git config --local --remove-section "$line"); done
+
+rm -rf .git\modules\.github && rm .github\.git
+rm -rf .git\modules\.vscode && rm .vscode\.git
 git commit -a -m "submodeles initial updates"
 git push
