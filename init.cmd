55d773cd6278f93152de1e998275d0056419a6bf
kirin123 <3436009+kirin123kirin@users.noreply.github.com>
2023-12-26 21:01:21 +0900

    Update init.cmd

--- init.cmd
+++ init.cmd
@@ -4,6 +4,7 @@ git submodule | cut -c43- | while read -r line; do (git rm "$line"); done
 # delete all submodule sections from .git/config (`git config --local --remove-section`) by fetching those from .git/config
 git config --local -l | grep submodule | sed -e 's/^\(submodule\.[^.]*\)\(.*\)/\1/g' | while read -r line; do (git config --local --remove-section "$line"); done
 
+# delete metadata
 rm -rf .git\modules\.github && rm .github\.git
 rm -rf .git\modules\.vscode && rm .vscode\.git
 git commit -a -m "submodeles initial updates"
